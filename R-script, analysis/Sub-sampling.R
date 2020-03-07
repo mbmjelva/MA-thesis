490b9c563d5145ac51cb65a6cd67da121e297af4
mbmjelva <mbmjelva@gmail.com>
2020-03-07 14:14:13 +0100

    Fixed sub-sampling

--- R-script, analysis/Sub-sampling.R
+++ R-script, analysis/Sub-sampling.R
@@ -2,26 +2,16 @@
 
 
 library(tidyverse)
-
-final <- read_rds("./Egne datasett/final_dataset.rds")
-
-final <- select(final, -best, -events, -agri_ih)
-
-
 library(devtools)
 source_gist("https://gist.github.com/mrdwab/6424112") # Må laste ned fra github-siden for å få riktig funksjon
 
-# Ønsker 500,000 totalt, så da må 75% av disse være 0 og 25% være 1. 
-500000*0.75 # 375000
-500000-375000
 
-s <- stratified(final, "conflict", size = c(375000, 125000), replace = T)
+final <- read_rds("./Egne datasett/final_dataset.rds")
 
-s
 
-# Blir riktig
+# Ønsker 1,000,000 totalt, så da må 75% av disse være 0 og 25% være 1. 
+s <- stratified(final, "conflict", size = c(750000, 250000), replace = T)
+
 table(s$conflict)/nrow(s)
 
-
-
-saveRDS(s, "./Egne datasett/resamled_data_25_vs_75.rds")
+saveRDS(s, "./Egne datasett/resampled_data.rds")
