3b08c0cc439dd215f90655d61642c8f0d383e21c
mbmjelva <mbmjelva@gmail.com>
2020-03-26 14:42:18 +0100

    Removed some of the non-substantial lag variables

--- R-script, analysis/Generalized random forest, lagged model.R
+++ R-script, analysis/Generalized random forest, lagged model.R
@@ -14,8 +14,13 @@ sample_final <- readRDS("./Egne datasett/resampled_data_lagged.rds")
 # Change to factor for later analysis
 sample_final$conflict <- as.factor(sample_final$conflict)
 
-# Remove the other conflict-variables. Here for robustness test (??)
-sample_final <- select(sample_final, -best, -events)
+# Remove:
+# - The other conflict-variables and their lags, will be used as alternative outcome variables for robustness checks
+# - The lagged geographical variables as they are not of substantial interest
+# - The lagged SPEI variables, they will be used as alternative treatment effects for robustness checks
+# - The full SPEI variable, might be used for robustness check?
+sample_final <- select(sample_final, -best, -events, -lag_1_best, -lag_1_events, lag_1_lon, lag_1_lat, lag_1_gwno, lag_1_bdist3, lag_1_capdist, lag_1_ttime_mean,
+                       -lag_1_spei3, -lag_1_spei3_neg, -lag_1_spei3_pos, -spei3)
 
 # Create training and test data set
 set.seed(125)
@@ -30,14 +35,11 @@ train_nona <- na.exclude(training)
 test_nona <- na.exclude(test)
 
 # Run seperate models with spei_neg and spei_pos as W. Must exclude the other variables to be able to run
-train_speipos <- select(train_nona, -spei3, -spei3_neg)
-train_speineg <- select(train_nona, -spei3, -spei3_pos)
+train_speipos <- select(train_nona, -spei3_neg)
+train_speineg <- select(train_nona, -spei3_pos)
 
-test_speipos <- select(test_nona, -spei3, -spei3_neg)
-test_speineg <- select(test_nona, -spei3, -spei3_pos)
-
-train_speifull <- select(train_nona, -spei3_pos, -spei3_neg)
-test_speifull <- select(test_nona, -spei3_pos, -spei3_neg)
+test_speipos <- select(test_nona, -spei3_neg)
+test_speineg <- select(test_nona, -spei3_pos)
 
 saveRDS(train_speipos, "./R-script, analysis/Models/train_speipos_lagged.rds")
 saveRDS(train_speineg, "./R-script, analysis/Models/train_speineg_lagged.rds")
@@ -45,19 +47,14 @@ saveRDS(train_speineg, "./R-script, analysis/Models/train_speineg_lagged.rds")
 saveRDS(test_speipos, "./R-script, analysis/Models/test_speipos_lagged.rds")
 saveRDS(test_speineg, "./R-script, analysis/Models/test_speineg_lagged.rds")
 
-saveRDS(train_speifull, "./R-script, analysis/Models/train_speifull_lagged.rds")
-saveRDS(test_speifull, "./R-script, analysis/Models/test_speifull_lagged.rds")
-
 # Run models --------------------------------------------------------------
-
-# Bør egentlig ha tune_parameter = "all" når kjører modellen, eller estimere mer enn 1000 trær.
 
 # Model with negative SPEI
 cf_neg <- causal_forest(
   X = model.matrix(~., data = train_speineg[, !(names(train_speineg) %in% c("conflict", "spei3_neg"))]), # exclude the outcome and treatment variables
   Y = as.numeric(train_speineg$conflict) - 1, # convert outcome to 0 or 1 (når gjør om til numeric vil levels bli 1 og 2, trekker fra 1 for å få 0 og 1 i stedet)
   W = train_speineg$spei3_neg[!is.na(train_speineg$spei3_neg)], # Must be without NA
-  tune.parameters = "all",
+  tune.parameters = "all", # Model tuens all tunable variables 
   seed = 2865
 )
 
@@ -69,7 +66,7 @@ varimp_neg <- cf_neg %>%
   variable_importance() %>% 
   as.data.frame() %>% 
   mutate(variable = colnames(cf_neg$X.orig)) %>% 
-  arrange(desc(V1)) # Endrer seg noe fra variable importance uten lagged. Unemployment er plutselig mye høyere opp
+  arrange(desc(V1))
 
 
 # Model with positive SPEI
@@ -91,38 +88,30 @@ varimp_pos <- cf_pos %>%
   arrange(desc(V1))
 
 
-# Model with full-scale SPEI
-cf_full <- causal_forest(
-  X = model.matrix(~., data = train_speipos[, !(names(train_speifull) %in% c("conflict", "spei3"))]), # exclude the outcome and treatment variables
-  Y = as.numeric(train_speifull$conflict) - 1, # convert outcome to 0 or 1 (når gjør om til numeric vil levels bli 1 og 2, trekker fra 1 for å få 0 og 1 i stedet)
-  W = train_speifull$spei3[!is.na(train_speifull$spei3)], # Must be without NA
-  tune.parameters = "all",
-  seed = 2865
-)
-
-save(cf_full, file = "./R-script, analysis/Models/cf_full_lagged.rds")
-
-varimp_full <- cf_full %>% 
-  variable_importance() %>% 
-  as.data.frame() %>% 
-  mutate(variable = colnames(cf_neg$X.orig)) %>% 
-  arrange(desc(V1))
-
 ## Plotter
 # SPEI_neg
 # Remove variables that are not explanatory so that not included in the plot
-varimp_neg_wo <- subset(varimp_neg, !variable %in% c("gid", "lon", "lat", "lag_1_lon", "lag_1_lat"))
+varimp_neg_wo <- subset(varimp_neg, !variable %in% c("gid", "lon", "lat"))
+
+area_color <- c("#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518",
+                "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518",
+                "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", 
+                "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518",
+                "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#3C1518", "#2D2A32")
+#Colors: #B77659, #3C1518, #2D2A32, #842F43
 
 (spei_neg <- ggplot(varimp_neg_wo) + 
-    geom_bar(aes(reorder(variable, V1), V1), stat = "identity") + # Reorder order the chategories depending on the values of a second variable (V1)
+    geom_bar(aes(reorder(variable, V1), V1), stat = "identity", fill = "#3C1518") + # Reorder order the chategories depending on the values of a second variable (V1)
     theme_minimal() +
     scale_y_continuous(limits = c(0, 0.2)) +
     labs(x = "", y = "Variable Importance", title = "SPEI3 negative") +
     coord_flip())
 
+ggsave("./Figurer/speineg_varimp.png")
+
 # SPEI_pos
 # Remove variables that are not explanatory so that not included in the plot
-varimp_pos_wo <- subset(varimp_pos, !variable %in% c("gid", "lon", "lat", "lag_1_lon", "lag_1_lat"))
+varimp_pos_wo <- subset(varimp_pos, !variable %in% c("gid", "lon", "lat", "lag_1_lon", "lag_1_lat", "lag_1_spei3", "lag_1_spei3_pos", "lag_1_spei3_neg"))
 
 (spei_pos <- ggplot(varimp_pos_wo) + 
   geom_bar(aes(reorder(variable, V1), V1), stat = "identity") + # Reorder order the chategories depending on the values of a second variable (V1)
