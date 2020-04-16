14d8377d7d2125be0b940de1f6e85b5dbc108f0c
mbmjelva <mbmjelva@gmail.com>
2020-04-16 15:04:31 +0200

    Removed missing SPEI values

--- R-script, analysis/Sub-sampling.R
+++ R-script, analysis/Sub-sampling.R
@@ -9,9 +9,18 @@ source_gist("https://gist.github.com/mrdwab/6424112") # Må laste ned fra github
 final <- read_rds("./Egne datasett/final_dataset.rds")
 
 
+# Create subsample with lagged variables only -----------------------------
+
+final_lagged <- select(final, starts_with("lag"), conflict)
+
+# Remove all cases that have NA on the SPEI-variable, so that the sub-sample contains enough SPEI values
+final_lagged <- final_lagged %>% filter(!is.na(lag_1_spei3))
+
+
 # Ønsker 1,000,000 totalt, så da må 75% av disse være 0 og 25% være 1. 
-s <- stratified(final, "conflict", size = c(750000, 250000), replace = T)
+s_l <- stratified(final_lagged, "conflict", size = c(750000, 250000), replace = T)
 
-table(s$conflict)/nrow(s)
+table(s_l$conflict)/nrow(s_l)
 
-saveRDS(s, "./Egne datasett/resampled_data.rds")
+saveRDS(s_l, "./Egne datasett/resampled_data_lagged.rds")
+
