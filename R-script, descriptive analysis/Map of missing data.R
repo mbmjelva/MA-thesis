ef0172030400fe593c73791d4543ce31ad86f9cb
mbmjelva <mbmjelva@gmail.com>
2020-04-20 11:23:20 +0200

    Changed some graphs

--- R-script, descriptive analysis/Map of missing data.R
+++ R-script, descriptive analysis/Map of missing data.R
@@ -35,6 +35,18 @@ ggplot() + geom_raster(data = final_miss3, aes(x= lon, y = lat, fill = pct_miss)
   theme_bw() +
   labs(title = "Percentage of missing SPEI values by country, after weighted by growing season")
 
+# SPEI3 variation
+ggplot(final) + geom_raster(aes(x = lon, y = lat, fill = spei3)) + 
+  geom_point(data = conf, aes(x = lon, y = lat, colour = "Conflict"), size = 0.5, alpha = 0.2) + 
+  coord_fixed(ratio = 1) +
+  scale_fill_viridis(direction = -1, begin = 0.4) +
+  theme_bw() +
+  scale_colour_manual(values = "#8E1D1D") +
+  labs(fill = "SPEI3", colour = element_blank()) +
+  theme_bw() +
+  theme(legend.position = c(fill = "bottom"),
+        legend.box = "vertical")
+  #labs(title = "Percentage of missing SPEI values by country, after weighted by growing season")
 
 
 # Conflict ----------------------------------------------------------------
@@ -45,12 +57,14 @@ conf <- final_miss2 %>% dplyr::select(lon, lat, conflict) %>% filter(conflict ==
 ggplot(final_miss2) + geom_raster(aes(x= lon, y = lat, fill = as.factor(pct_miss))) + 
   geom_point(data = conf, aes(x = lon, y = lat, colour = "Conflict"), size = 0.5, alpha = 0.5) + 
   coord_fixed(ratio = 1) +
-  scale_fill_manual(values = c("#9AE5E6", "#008279"), labels = c("Not missing", "Missing")) +
-  scale_colour_manual(values = "black") +
+  scale_fill_manual(values = c("#9AE5E6", "darkgrey"), labels = c("Not missing", "Missing")) +
+  scale_colour_manual(values = "#8E1D1D") +
   labs(fill = "SPEI3", colour = element_blank()) +
   theme_bw() +
   theme(legend.position = c(fill = "bottom"),
         legend.box = "vertical")
+
+#values = c("#9AE5E6", "#008279")
 
 # Conflict blank
 ggplot(final_miss2) + geom_raster(aes(x= lon, y = lat), fill = "#4EAAAD") + 
@@ -66,41 +80,65 @@ ggsave("./Figurer/conflict geographic distribution.png")
 # Other variables with much missing ---------------------------------------
 
 # Unemployment
+gg_exc_unempl <- ggplotGrob(ggplot(final, aes(unempl_tot)) + 
+  geom_histogram(fill = viridis(30, direction = -1)) + 
+  theme_bw() + 
+  labs(x = "", y = ""))
+
 ggplot(final) + geom_raster(aes(x = lon, y = lat, fill = unempl_tot)) + 
   coord_fixed(ratio = 1) +
   theme_bw() +
   scale_fill_viridis(direction = -1) +
-  labs(fill = "Unemployment")
+  labs(fill = "Unemployment") +
+  annotation_custom(gg_exc_unempl, xmin = -200, xmax = -90, ymin = -65, ymax = 0)
+
+ggsave("./Figurer/geogr_dist_unempl.png")
 
 # Excluded
-gg_exc_map <- ggplot(final) + geom_raster(aes(x= lon, y = lat, fill = as.factor(excluded))) + 
-  #geom_bar(aes(excluded, fill = as.factor(excluded))) + 
+gg_exc_bar <- ggplotGrob(ggplot(final) + geom_bar(aes(as.factor(excluded), fill = as.factor(excluded))) + 
+                           theme_bw() + 
+                           theme(legend.position = "none") +
+                           scale_fill_viridis_d(direction = -1, na.value = "grey50") +
+                           labs(x = "", y = ""))
+
+ggplot(final) + geom_raster(aes(x= lon, y = lat, fill = as.factor(excluded))) + 
   coord_fixed(ratio = 1) +
   theme_bw() + 
   labs(fill = "Excluded") +
-  scale_fill_viridis_d(direction = -1, na.value = "grey50")
-
-gg_exc_bar <- ggplot(final) + geom_bar(aes(as.factor(excluded), fill = as.factor(excluded))) + 
-  theme_bw() + 
-  theme(legend.position = "none") +
   scale_fill_viridis_d(direction = -1, na.value = "grey50") +
-  labs(x = "Excluded", y = "")
+  annotation_custom(gg_exc_bar, xmin = -200, xmax = -90, ymin = -65, ymax = 0)
 
-ggarrange(gg_exc_map, gg_exc_bar, nrow = 2)
+ggsave("./Figurer/geogr_dist_excluded.png")
 
 
 # SHDI
+gg_shdi_bar <- ggplotGrob(ggplot(final, aes(shdi)) + 
+  geom_histogram(fill = viridis(30)) + 
+  theme_bw() + 
+  labs(x = "", y = ""))
+
 ggplot(final) + geom_raster(aes(x= lon, y = lat, fill = shdi)) + 
   coord_fixed(ratio = 1) +
   theme_bw() + 
-  scale_fill_viridis()
+  labs(fill = "SHDI") +
+  scale_fill_viridis() +
+  annotation_custom(gg_shdi_bar, xmin = -200, xmax = -90, ymin = -65, ymax = 0)
+
+ggsave("./Figurer/geogr_dist_shdi.png")
 
 # Agriculture in cell
+gg_agri_bar <- ggplotGrob(ggplot(final, aes(agri_ih)) + 
+    geom_histogram(fill = viridis(30)) + 
+    theme_bw() + 
+    labs(x = "", y = ""))
+
 ggplot(final) + geom_raster(aes(x= lon, y = lat, fill = agri_ih)) +
   coord_fixed(ratio = 1) +
   theme_bw() +
-  scale_fill_viridis(direction = -1)
+  labs(fill = "Percentage of\nagricultural\narea in cell") +
+  scale_fill_viridis() +
+  annotation_custom(gg_agri_bar, xmin = -200, xmax = -90, ymin = -65, ymax = 0)
 
-
+ggsave("./Figurer/geogr_dist_agri.png")
 
 
