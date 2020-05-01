c3aac80863259cdbab98e92ea0602c6b02d91d30
mbmjelva <mbmjelva@gmail.com>
2020-05-01 16:17:33 +0200

    Removed unempl_tot, kept static vars as not lagged

--- R-script, analysis/Sub-sampling.R
+++ R-script, analysis/Sub-sampling.R
@@ -10,8 +10,10 @@ final <- read_rds("./Egne datasett/final_dataset.rds")
 
 
 # Create subsample with lagged variables only -----------------------------
+#' Keep the static variables as they are so that do not miss information from 1989 when lagged (gwno has been static since 2011)
+#' Remove the lagged versions of the static variables. Also remove unemployment, as has a large amount of missing (almost all of Africa is missing)
 
-final_lagged <- select(final, starts_with("lag"), conflict)
+final_lagged <- select(final, gid, lon, lat, year, gwno, conflict, ttime_mean, starts_with("lag"), -lag_1_lon, -lag_1_lat, -lag_1_gwno, -lag_1_ttime_mean, -lag_1_unempl_tot, -unempl_tot)
 
 # Remove all cases that have NA on the SPEI-variable, so that the sub-sample contains enough SPEI values
 final_lagged <- final_lagged %>% filter(!is.na(lag_1_spei3))
