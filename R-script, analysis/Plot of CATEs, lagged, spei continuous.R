9dd84baa932f5a939ccb5f9473780fff86815489
mbmjelva <mbmjelva@gmail.com>
2020-05-06 09:27:48 +0200

    Added CATEs for continuous spei

---
+++ R-script, analysis/Plot of CATEs, lagged, spei continuous.R
@@ -0,0 +1,98 @@
+## CATEs, SPEI continuous, lagged ##
+
+library(tidyverse)
+
+# Spei neg
+load(file = "./R-script, analysis/Models/cates_neg_with_target.rds")
+
+cates_neg %>% filter(variable %in% c("libdem", "temp", "capdist", "bdist", "ttime_mean", "global_ind", "pop")) %>% 
+  ggplot(aes(y = as.factor(round(quantile, digits = 1)), x = ape, xmin = apemin, xmax = apemax, color = significant)) + 
+  facet_grid(vars(variable), scales = "free") +
+  geom_point() + 
+  geom_errorbar() + 
+  theme_light() +
+  scale_x_continuous(limits = c(-0.085, 0.015), breaks = seq(-0.08, 0.01, 0.01)) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    strip.text.y = element_text(colour = "black"),
+    strip.background = element_rect(colour = NA, fill = NA),
+    legend.position = "bottom",
+    text = element_text(size = 20)
+  ) +
+  scale_color_manual(values = c("no" = "#3C1518", "yes" = "#008279")) +
+  labs(y = "", x = "CATE, SPEI3 neg") +
+  geom_vline(xintercept = 0, linetype = 3)
+
+
+ggsave("./Figurer/cates_neg, spei cont, target, seven variables 1.png", height = 15, width = 11)
+
+cates_neg %>% filter(variable %in% c("gdp", "shdi", "empl_agr", "irrig_sum", "agri_ih", "excluded", "lag_conflict", "non_state_conflict")) %>% 
+  ggplot(aes(y = as.factor(round(quantile, digits = 1)), x = ape, xmin = apemin, xmax = apemax, color = significant)) + 
+  facet_grid(vars(variable), scales = "free") +
+  geom_point() + 
+  geom_errorbar() + 
+  theme_light() +
+  scale_x_continuous(limits = c(-0.085, 0.015), breaks = seq(-0.08, 0.01, 0.01)) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    strip.text.y = element_text(colour = "black"),
+    strip.background = element_rect(colour = NA, fill = NA),
+    legend.position = "bottom",
+    text = element_text(size = 20)
+  ) +
+  scale_color_manual(values = c("no" = "#3C1518", "yes" = "#008279")) +
+  labs(y = "", x = "CATE, CF-neg") +
+  geom_vline(xintercept = 0, linetype = 3)
+
+ggsave("./Figurer/cates_neg, spei cont, target, seven variables 2.png", height = 15, width = 11)
+
+
+# Spei pos
+load(file = "./R-script, analysis/Models/cates_pos_with_target.rds")
+
+cates_pos %>% filter(variable %in% c("libdem", "temp", "capdist", "bdist", "ttime_mean", "global_ind", "pop")) %>% 
+  ggplot(aes(y = as.factor(round(quantile, digits = 1)), x = ape, xmin = apemin, xmax = apemax, color = significant)) + 
+  facet_grid(vars(variable), scales = "free") +
+  geom_point() + 
+  geom_errorbar() + 
+  theme_light() +
+  scale_x_continuous(limits = c(-0.085, 0.015), breaks = seq(-0.08, 0.01, 0.01)) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    strip.text.y = element_text(colour = "black"),
+    strip.background = element_rect(colour = NA, fill = NA),
+    legend.position = "bottom",
+    text = element_text(size = 20)
+  ) +
+  scale_color_manual(values = c("no" = "#3C1518", "yes" = "#008279")) +
+  labs(y = "", x = "CATE, CF-pos") +
+  geom_vline(xintercept = 0, linetype = 3)
+
+
+ggsave("./Figurer/cates_pos, spei cont, target, seven variables 1.png",  height = 15, width = 11)
+
+#labels <- c("Agricultural area\in cell", "Employment\agriculture", "Excluded", "GDP", "Irrigation", "Conflict\lagged", "Non-state\conflict", "SHDI")
+
+cates_pos %>% filter(variable %in% c("gdp", "shdi", "empl_agr", "irrig_sum", "agri_ih", "excluded", "lag_conflict", "non_state_conflict")) %>% 
+  ggplot(aes(y = as.factor(round(quantile, digits = 1)), x = ape, xmin = apemin, xmax = apemax, color = significant)) + 
+  facet_grid(vars(variable), scales = "free") +
+  geom_point() + 
+  geom_errorbar() + 
+  theme_light() +
+  scale_x_continuous(limits = c(-0.085, 0.015), breaks = seq(-0.08, 0.01, 0.01)) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    strip.text.y = element_text(colour = "black"),
+    strip.background = element_rect(colour = NA, fill = NA),
+    legend.position = "bottom",
+    text = element_text(size = 20)
+  ) +
+  scale_color_manual(values = c("no" = "#3C1518", "yes" = "#008279")) +
+  labs(y = "", x = "CATE, CF-pos") +
+  geom_vline(xintercept = 0, linetype = 3)
+
+ggsave("./Figurer/cates_pos, speidich, target, seven variables 2.png", height = 15, width = 11)
