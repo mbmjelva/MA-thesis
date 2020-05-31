37832fb06693df194c2bcac72187c04229f0c666
mbmjelva <mbmjelva@gmail.com>
2020-05-31 20:45:21 +0200

    Added continent-specific blp

--- R-script, analysis/Best linear projection.R
+++ R-script, analysis/Best linear projection.R
@@ -12,42 +12,128 @@ train_speineg <- readRDS(file = "./R-script, analysis/Models/train_speineg_lagge
 train_speipos <- readRDS(file = "./R-script, analysis/Models/train_speipos_lagged_speidich_only_conflict.rds")
 
 # Estimate the best linear projection of the CATEs
-blp_neg <- best_linear_projection(cf_neg_speidich, 
+
+blp_neg_cate <- best_linear_projection(cf_neg_speidich, 
+                                       A = train_speineg[, (names(train_speineg) %in% 
+                                                              c("shdi", "agri_ih", "empl_agr", 
+                                                                "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                "temp"))])
+blp_pos_cate <- best_linear_projection(cf_pos_speidich, 
+                                       A = train_speipos[, (names(train_speipos) %in% 
+                                                              c("shdi", "agri_ih", "empl_agr", 
+                                                                "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                "temp"))])
+
+blp_neg_catt <- best_linear_projection(cf_neg_speidich, 
                                   A = train_speineg[, (names(train_speineg) %in% 
                                                          c("shdi", "agri_ih", "empl_agr", 
                                                            "irrig_sum", "bdist3", "capdist", "ttime_mean", 
                                                            "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
-                                                           "temp"))])
-blp_pos <- best_linear_projection(cf_pos_speidich, 
+                                                           "temp"))],
+                                  subset = train_speineg$spei3_neg > 0)
+blp_pos_catt <- best_linear_projection(cf_pos_speidich, 
                                   A = train_speipos[, (names(train_speipos) %in% 
                                                                           c("shdi", "agri_ih", "empl_agr", 
                                                                             "irrig_sum", "bdist3", "capdist", "ttime_mean", 
                                                                             "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
-                                                                            "temp"))])
+                                                                            "temp"))],
+                                  subset = train_speipos$spei3_pos > 0)
 
-stargazer(blp_neg, blp_pos, type = "text",
-          column.labels = c("CF-neg", "CF-pos"),
+stargazer(blp_neg_catt, blp_neg_cate, blp_pos_catt, blp_pos_cate, #type = "text",
+          column.labels = c("CF-neg CATT", "CF-neg CATE", "CF-pos CATT", "CF-pos CATE"),
           dep.var.caption = "")
 
-# Check if effects change much if exclude some of the variables
-blp_neg_selected <- best_linear_projection(cf_neg_speidich, 
-                                           A = train_speineg[, (names(train_speineg) %in% 
-                                                                  c("shdi", "agri_ih", 
-                                                                    "capdist", "ttime_mean", 
-                                                                    "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
-                                                                    "temp"))])
 
-blp_pos_selected <- best_linear_projection(cf_pos_speidich, 
-                                  A = train_speipos[, (names(train_speipos) %in% 
-                                                         c("shdi", "agri_ih", 
-                                                           "capdist", "ttime_mean", 
-                                                           "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
-                                                           "temp"))])
+# Check if vary per continent ---------------------------------------------
 
-stargazer(blp_neg_selected, blp_pos_selected, blp_neg, blp_pos, type = "text",
-          column.labels = c("CF-neg", "CF-pos"),
-          dep.var.caption = "", column.sep.width = "-15pt")
+# Africa
+blp_neg_catt_af <- best_linear_projection(cf_neg_speidich, 
+                                       A = train_speineg[, (names(train_speineg) %in% 
+                                                              c("shdi", "agri_ih", "empl_agr", 
+                                                                "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                "temp"))],
+                                       subset = c(train_speineg$spei3_neg > 0 & train_speineg$continent == "Africa"))
+blp_pos_catt_af <- best_linear_projection(cf_pos_speidich, 
+                                       A = train_speipos[, (names(train_speipos) %in% 
+                                                              c("shdi", "agri_ih", "empl_agr", 
+                                                                "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                "temp"))],
+                                       subset = c(train_speipos$spei3_pos > 0 & train_speipos$continent == "Africa")) 
 
-# Not much change 
+# Asia
+blp_neg_catt_as <- best_linear_projection(cf_neg_speidich, 
+                                          A = train_speineg[, (names(train_speineg) %in% 
+                                                                 c("shdi", "agri_ih", "empl_agr", 
+                                                                   "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                   "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                   "temp"))],
+                                          subset = c(train_speineg$spei3_neg > 0 & train_speineg$continent == "Asia"))
+blp_pos_catt_as <- best_linear_projection(cf_pos_speidich, 
+                                          A = train_speipos[, (names(train_speipos) %in% 
+                                                                 c("shdi", "agri_ih", "empl_agr", 
+                                                                   "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                   "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                   "temp"))],
+                                          subset = c(train_speipos$spei3_pos > 0 & train_speipos$continent == "Asia")) 
+# Americas
+blp_neg_catt_am <- best_linear_projection(cf_neg_speidich, 
+                                          A = train_speineg[, (names(train_speineg) %in% 
+                                                                 c("shdi", "agri_ih", "empl_agr", 
+                                                                   "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                   "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                   "temp"))],
+                                          subset = c(train_speineg$spei3_neg > 0 & train_speineg$continent == "Americas"))
+blp_pos_catt_am <- best_linear_projection(cf_pos_speidich, 
+                                          A = train_speipos[, (names(train_speipos) %in% 
+                                                                 c("shdi", "agri_ih", "empl_agr", 
+                                                                   "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                   "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                   "temp"))],
+                                          subset = c(train_speipos$spei3_pos > 0 & train_speipos$continent == "Americas")) 
 
+# Europe
+blp_neg_catt_eu <- best_linear_projection(cf_neg_speidich, 
+                                          A = train_speineg[, (names(train_speineg) %in% 
+                                                                 c("shdi", "agri_ih", "empl_agr", 
+                                                                   "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                   "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                   "temp"))],
+                                          subset = c(train_speineg$spei3_neg > 0 & train_speineg$continent == "Europe"))
+blp_pos_catt_eu <- best_linear_projection(cf_pos_speidich, 
+                                          A = train_speipos[, (names(train_speipos) %in% 
+                                                                 c("shdi", "agri_ih", "empl_agr", 
+                                                                   "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                   "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                   "temp"))],
+                                          subset = c(train_speipos$spei3_pos > 0 & train_speipos$continent == "Europe")) 
+
+# Oceania
+
+blp_neg_catt_oc <- best_linear_projection(cf_neg_speidich, 
+                                          A = train_speineg[, (names(train_speineg) %in% 
+                                                                 c("shdi", "agri_ih", "empl_agr", 
+                                                                   "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                   "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                   "temp"))],
+                                          subset = c(train_speineg$spei3_neg > 0 & train_speineg$continent == "Oceania"))
+
+blp_pos_catt_oc <- best_linear_projection(cf_pos_speidich, 
+                                          A = train_speipos[, (names(train_speipos) %in% 
+                                                                 c("shdi", "agri_ih", "empl_agr", 
+                                                                   "irrig_sum", "bdist3", "capdist", "ttime_mean", 
+                                                                   "pop", "excluded", "global_ind", "gdp", "non_state_conflict",
+                                                                   "temp"))],
+                                          subset = c(train_speipos$spei3_pos > 0 & train_speipos$continent == "Oceania")) 
+
+stargazer(blp_neg_catt_af, blp_neg_catt_as, blp_neg_catt_am, blp_neg_catt_eu, blp_neg_catt_oc, #type = "text",
+          column.labels = c("Africa", "Asia", "America", "Europe", "Oceania"), dep.var.caption = "CATT",
+          title = "Best linear projection of the CATT, negative SPEI3")
+
+stargazer(blp_pos_catt_af, blp_pos_catt_as, blp_pos_catt_am, blp_pos_catt_eu, blp_pos_catt_oc, #type = "text",
+          column.labels = c("Africa", "Asia", "America", "Europe", "Oceania"), dep.var.caption = "CATT",
+          title = "Best linear projection of the CATT, positive SPEI3")
 
