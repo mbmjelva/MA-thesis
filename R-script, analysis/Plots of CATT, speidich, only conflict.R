cdcb8567a99d3c3f16cb4783a3653eb69e1145d3
mbmjelva <mbmjelva@gmail.com>
2020-06-03 13:22:18 +0200

    Added plot w vars with heterogeneity only

--- R-script, analysis/Plots of CATT, speidich, only conflict.R
+++ R-script, analysis/Plots of CATT, speidich, only conflict.R
@@ -2,8 +2,78 @@
 
 library(tidyverse)
 
-# Spei neg
+# Load files
 load(file = "./R-script, analysis/Models/catt_neg_speidich_only_conflict.rds")
+load(file = "./R-script, analysis/Models/catt_pos_speidich_only_conflict.rds")
+
+
+# Plots with variables with heterogeneity ---------------------------------
+catt_neg %>% filter(variable %in% c("bdist3", "pop", "shdi", "temp", "ttime_mean")) %>% 
+  ggplot(aes(y = as.factor(round(quantile, digits = 1)), x = catt, xmin = cattmin, xmax = cattmax, color = significant)) + 
+  facet_grid(vars(variable), scales = "free") +
+  geom_point() + 
+  geom_errorbar() + 
+  theme_light() +
+  scale_x_continuous(limits = c(-0.1, 0.21), breaks = seq(-0.1, 0.21, 0.05)) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    strip.text.y = element_text(colour = "black"),
+    strip.background = element_rect(colour = NA, fill = NA),
+    legend.position = "bottom",
+    text = element_text(size = 20)
+  ) +
+  scale_color_manual(values = c("no" = "#3C1518", "yes" = "#008279")) +
+  labs(y = "", x = "CATT, CF-neg") +
+  geom_vline(xintercept = 0, linetype = 3)
+
+
+ggsave("./Figurer/catt_neg, speidich, only conflict, vars with het.png")
+
+
+catt_pos %>% filter(variable %in% c("agri_ih", "bdist3", "capdist", "empl_agr", "excluded", "gdp")) %>% 
+  ggplot(aes(y = as.factor(round(quantile, digits = 1)), x = catt, xmin = cattmin, xmax = cattmax, color = significant)) + 
+  facet_grid(vars(variable), scales = "free") +
+  geom_point() + 
+  geom_errorbar() + 
+  theme_light() +
+  scale_x_continuous(limits = c(-0.1, 0.21), breaks = seq(-0.1, 0.21, 0.05)) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    strip.text.y = element_text(colour = "black"),
+    strip.background = element_rect(colour = NA, fill = NA),
+    legend.position = "bottom",
+    text = element_text(size = 20)
+  ) +
+  scale_color_manual(values = c("no" = "#3C1518", "yes" = "#008279")) +
+  labs(y = "", x = "CATT, CF-pos") +
+  geom_vline(xintercept = 0, linetype = 3)
+
+ggsave("./Figurer/catt_pos, speidich, only conflict, vars with het 1.png")
+
+catt_pos %>% filter(variable %in% c("global_ind", "irrig_sum", "libdem", "pop",  "temp", "ttime_mean")) %>% 
+  ggplot(aes(y = as.factor(round(quantile, digits = 1)), x = catt, xmin = cattmin, xmax = cattmax, color = significant)) + 
+  facet_grid(vars(variable), scales = "free") +
+  geom_point() + 
+  geom_errorbar() + 
+  theme_light() +
+  scale_x_continuous(limits = c(-0.1, 0.21), breaks = seq(-0.1, 0.21, 0.05)) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    strip.text.y = element_text(colour = "black"),
+    strip.background = element_rect(colour = NA, fill = NA),
+    legend.position = "bottom",
+    text = element_text(size = 20)
+  ) +
+  scale_color_manual(values = c("no" = "#3C1518", "yes" = "#008279")) +
+  labs(y = "", x = "CATT, CF-pos") +
+  geom_vline(xintercept = 0, linetype = 3)
+
+ggsave("./Figurer/catt_pos, speidich, only conflict, vars with het 2.png")
+
+# Plots with all variables (for appendix) ---------------------------------
 
 catt_neg %>% filter(variable %in% c("libdem", "temp", "capdist", "bdist3", "ttime_mean", "global_ind", "pop")) %>% 
   ggplot(aes(y = as.factor(round(quantile, digits = 1)), x = catt, xmin = cattmin, xmax = cattmax, color = significant)) + 
@@ -49,7 +119,6 @@ ggsave("./Figurer/catt_neg, speidich, only conflict, seven variables 2.png", hei
 
 
 # Spei pos
-load(file = "./R-script, analysis/Models/cates_pos_speidich_only_conflict.rds")
 
 catt_pos %>% filter(variable %in% c("libdem", "temp", "capdist", "bdist3", "ttime_mean", "global_ind", "pop")) %>% 
   ggplot(aes(y = as.factor(round(quantile, digits = 1)), x = catt, xmin = cattmin, xmax = cattmax, color = significant)) + 
