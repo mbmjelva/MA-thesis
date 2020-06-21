40beb75a143404e3fe1274170d2d4e80fb662b96
mbmjelva <mbmjelva@gmail.com>
2020-06-21 17:15:58 +0200

    Updated script for estimation of CATEs

--- R-script, analysis/Estimation of CATEs/Estimate CATEs, lagged, spei continuous.R
+++ R-script, analysis/Estimation of CATEs/Estimate CATEs, lagged, spei continuous.R
@@ -1,7 +1,9 @@
 ### Estimation of CATEs ###
-# Lagged, spei continuous
 
-# Using averatge_treatment_effect
+# SPEI continuous
+
+# This script estimates the CATEs for different subgroups of the population with continuous treatment
+# Prepares the data to be plottet for the plots of CATEs in the analysis and appendices
 
 
 library(tidyverse)
@@ -12,40 +14,42 @@ load(file = "./R-script, analysis/Models/cf_neg_lagged.rds")
 train_speineg <- readRDS(file = "./R-script, analysis/Models/train_speineg_lagged.rds")
 test_speineg <- readRDS(file = "./R-script, analysis/Models/test_speineg_lagged.rds")
 
-# Tester med numerisk variabel
+# Make variable numeric
 test_speineg$lag_conflict <- as.numeric(as.character(test_speineg$lag_conflict))
 train_speineg$lag_conflict <- as.numeric(as.character(train_speineg$lag_conflict))
 
 test_speineg <- dplyr::select(test_speineg, -conflict, -spei3_neg, -lon, -lat, -gwno)# Must remove the cluster-variable and treatment and outcome
 train_speineg <- dplyr::select(train_speineg, -conflict, -spei3_neg, -lon, -lat, -gwno)
 
+area_of_overlap <- cf_neg$W.hat > 0.1 & cf_neg$W.hat < 0.9
+
 cates_neg <- tibble()
 
 vars <- names(test_speineg)
 
 for(var in vars){
-  var_quantiles <- quantile(train_speineg[[var]], probs = seq(0.05, 0.95, 0.25))
+  var_quantiles <- quantile(train_speineg[[var]], probs = seq(0.05, 0.95, 0.25), na.rm = T)
   for(i in 1:length(var_quantiles)){
     qtl <- var_quantiles[i]
     qtl_l <- var_quantiles[i-1]
     if(i == 1){
-      res <- average_treatment_effect(cf_neg, subset = train_speineg[[var]] <= qtl, target.sample = "overlap")
+      res <- average_partial_effect(cf_neg, subset = (area_of_overlap & (train_speineg[[var]] <= qtl)))
     } else if (qtl == qtl_l) {
-      res <- average_treatment_effect(cf_neg, subset = train_speineg[[var]] <= qtl, target.sample = "overlap")
+      res <- average_partial_effect(cf_neg, subset = (area_of_overlap & (train_speineg[[var]] <= qtl)))
     } else {
-      res <- average_treatment_effect(cf_neg, subset = (train_speineg[[var]] <= qtl) & (train_speineg[[var]] > qtl_l), target.sample = "overlap")
+      res <- average_partial_effect(cf_neg, subset = (area_of_overlap & (train_speineg[[var]] <= qtl) & (train_speineg[[var]] > qtl_l)))
     }
-    cates_neg <- bind_rows(cates_neg, tibble("variable" = var, "quantile" = qtl, "ape" = res[1], "ape_std" = res[2]))
+    cates_neg <- bind_rows(cates_neg, tibble("variable" = var, "quantile" = qtl, "cate" = res[1], "cate_std" = res[2]))
   }
 }
 
 
-cates_neg$apemin <- cates_neg$ape - (1.96*cates_neg$ape_std)
-cates_neg$apemax <- cates_neg$ape + (1.96*cates_neg$ape_std)
+cates_neg$catemin <- cates_neg$cate - (1.96*cates_neg$cate_std)
+cates_neg$catemax <- cates_neg$cate + (1.96*cates_neg$cate_std)
 
-cates_neg$significant <- if_else(cates_neg$apemin > 0 | cates_neg$apemax < 0, "yes", "no")
+cates_neg$significant <- if_else(cates_neg$catemin > 0 | cates_neg$catemax < 0, "yes", "no")
 
-save(cates_neg, file = "./R-script, analysis/Models/cates_neg_with_target.rds")
+save(cates_neg, file = "./R-script, analysis/Models/cates_neg_with_overlap, continuous.rds")
 
 
 
@@ -63,30 +67,32 @@ train_speipos$lag_conflict <- as.numeric(as.character(train_speipos$lag_conflict
 test_speipos <- dplyr::select(test_speipos, -conflict, -spei3_pos, -lon, -lat, -gwno)# Must remove the cluster-variable and treatment and outcome
 train_speipos <- dplyr::select(train_speipos, -conflict, -spei3_pos, -lon, -lat, -gwno)
 
+area_of_overlap_pos <- cf_pos$W.hat > 0.1 & cf_pos$W.hat < 0.9
+
 cates_pos <- tibble()
 
 vars <- names(test_speipos)
 
 for(var in vars){
-  var_quantiles <- quantile(train_speipos[[var]], probs = seq(0, 1, 0.25)) # Set to vary between lowest and highest values of the variables
+  var_quantiles <- quantile(train_speipos[[var]], probs = seq(0.05, 0.95, 0.25), na.rm = T)
   for(i in 1:length(var_quantiles)){
     qtl <- var_quantiles[i]
     qtl_l <- var_quantiles[i-1]
     if(i == 1){
-      res <- average_treatment_effect(cf_pos, subset = train_speipos[[var]] <= qtl, target.sample = "overlap")
+      res <- average_partial_effect(cf_pos, subset = (area_of_overlap & (train_speipos[[var]] <= qtl)))
     } else if (qtl == qtl_l) {
-      res <- average_treatment_effect(cf_pos, subset = train_speipos[[var]] <= qtl, target.sample = "overlap")
+      res <- average_partial_effect(cf_pos, subset = (area_of_overlap & (train_speipos[[var]] <= qtl)))
     } else {
-      res <- average_treatment_effect(cf_pos, subset = (train_speipos[[var]] <= qtl) & (train_speipos[[var]] > qtl_l), target.sample = "overlap")
+      res <- average_partial_effect(cf_pos, subset = (area_of_overlap & (train_speipos[[var]] <= qtl) & (train_speipos[[var]] > qtl_l)))
     }
-    cates_pos <- bind_rows(cates_pos, tibble("variable" = var, "quantile" = qtl, "ape" = res[1], "ape_std" = res[2]))
+    cates_pos <- bind_rows(cates_pos, tibble("variable" = var, "quantile" = qtl, "cate" = res[1], "cate_std" = res[2]))
   }
 }
 
 
-cates_pos$apemin <- cates_pos$ape - (1.96*cates_pos$ape_std)
-cates_pos$apemax <- cates_pos$ape + (1.96*cates_pos$ape_std)
+cates_pos$catemin <- cates_pos$cate - (1.96*cates_pos$cate_std)
+cates_pos$catemax <- cates_pos$cate + (1.96*cates_pos$cate_std)
 
-cates_pos$significant <- if_else(cates_pos$apemin > 0 | cates_pos$apemax < 0, "yes", "no")
+cates_pos$significant <- if_else(cates_pos$catemin > 0 | cates_pos$catemax < 0, "yes", "no")
 
-save(cates_pos, file = "./R-script, analysis/Models/cates_pos_with_target.rds")
+save(cates_pos, file = "./R-script, analysis/Models/cates_pos_overlap_continuous.rds")
